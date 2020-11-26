a154736afd441c79e630935fded86df497afa933
morshc45 <morshc45@gmail.com>
2020-11-26 21:12:22 +0200

    (update) create index page and post page

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .services import posts
+
+
+def index(request):
+    posts_list = posts.get_posts_list()
+
+    return render(request, 'index.twig', {
+        'posts_list': posts_list
+    })
+
+
+def post_page(request, post_id):
+    post = posts.get_post_info_by_id(post_id)
+
+    return render(request, 'post.twig', {
+        'post': post
+    })
