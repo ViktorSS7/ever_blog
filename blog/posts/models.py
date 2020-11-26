c46bf71c849f4dd054bb6bf8ee73e4955ed64bf1
morshc45 <morshc45@gmail.com>
2020-11-26 19:49:57 +0200

    (update) register models to admin

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -7,9 +7,16 @@ class Post(models.Model):
     body = models.TextField(max_length=65000)
     date_added = models.DateTimeField('publication date')
 
+    def __str__(self):
+        return self.title
+
 
 class Comment(models.Model):
 
     author_name = models.CharField(max_length=255)
     body = models.TextField(max_length=1000)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, default=None)
     date_added = models.DateTimeField('publication date')
+
+    def __str__(self):
+        return str(self.author_name) + ' - ' + self.date_added.strftime('%d.%m.%Y %H:%M')
