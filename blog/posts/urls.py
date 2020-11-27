23132873ce4d3d793ab5fe9d8a42e99186669306
morshc45 <morshc45@gmail.com>
2020-11-27 15:58:00 +0200

    (update) add comments form

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='posts'),
-    path('<int:post_id>/', views.post_page, name='post_page')
+    path('<int:post_id>/', views.post_page, name='post_page'),
+
+    path('comment/<int:post_id>/post', views.comment_create, name='comment_create')
 ]
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
-from .services import posts
+from django.template import loader
+
+from django.http import HttpResponseRedirect, HttpResponse
+
+from django.urls import reverse
+
+from .services import posts, comments
+
+from .forms import CommentCreateForm
 
 
 def index(request):
@@ -14,6 +22,25 @@ def index(request):
 def post_page(request, post_id):
     post = posts.get_post_info_by_id(post_id)
 
+    comment_form = CommentCreateForm()
+    comments_list = comments.get_comments_list_by_post_id(post_id)
+    comments_output = loader.get_template('comments.twig').render({
+        'comments_list': comments_list,
+        'post_id': post_id,
+        'comment_form': comment_form
+    }, request)
+
     return render(request, 'post.twig', {
-        'post': post
+        'post': post,
+        'comments_list': comments_output
     })
+
+
+def comment_create(request, post_id):
+    if request.method == 'POST':
+        form = CommentCreateForm(request.POST)
+
+        if form.is_valid():
+            comments.add_comment_to_post(post_id, form.cleaned_data['your_name'], form.cleaned_data['comment'])
+
+    return HttpResponseRedirect(reverse('post_page', args=[post_id]))
